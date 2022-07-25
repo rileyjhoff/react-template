f9fed2672987fd799bc958b6eb8a5cbeb97e51b5
Riley Hoffman <rileyjhoff@gmail.com>
2022-07-25 15:17:59 -0700

    add List and About pages with routes

---
+++ src/components/About/About.jsx
@@ -0,0 +1,9 @@
+import styles from './About.css';
+
+export default function About() {
+  return (
+    <div className={styles.About}>
+      <p>Hello World from the About Page</p>
+    </div>
+  );
+}
