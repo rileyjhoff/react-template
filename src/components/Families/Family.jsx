b2c9e4c9d725c58ef9aee4d89ceab3273e73234c
Riley Hoffman <rileyjhoff@gmail.com>
2022-08-02 16:01:27 -0700

    add ability to update family

--- src/components/Families/Family.jsx
+++ src/components/Families/Family.jsx
@@ -1,15 +1,55 @@
+import { useState } from 'react';
 import { useActions } from '../../state/hooks/families';
+import { InputControl } from '../Forms/FormControls';
 import styles from './Family.css';
 
 export default function Family({ family }) {
-  const { remove } = useActions();
-  
-  const handleRemove = () => remove(family.id);
+  const { remove, update } = useActions();
+  const [editing, setEditing] = useState(false);
+  const [name, setName] = useState(family.name);
+
+  const handleRemove = async () => {
+    const warning = `Are you sure you want to remove ${family.name}?`;
+    if (confirm(warning)) {
+      await remove(family.id);
+    }
+  };
+
+  const handleEditStart = () => setEditing(true);
+
+  const handleChange = (e) => setName(e.target.value);
+
+  const handleEditFinish = async () => {
+    setEditing(false);
+    if (name === family.name) return;
+    await update({ family, ...name });
+  };
+
+  const handleKeyUp = (e) => {
+    if (e.code === 'Enter') handleEditFinish();
+  };
+
 
   return (
     <li className={styles.Family}>
-      <h2>{family.name}</h2>
-      <button onClick={handleRemove}>ⓧ</button>
+      {editing ? (
+        <>
+          <button onClick={handleEditFinish}>✅</button>
+          <InputControl 
+            name="edit-family-name"
+            value={name}
+            onChange={handleChange}
+            onKeyUp={handleKeyUp}
+            className={styles.EditInput}
+          />
+        </>
+      ) : (
+        <>
+          <button onClick={handleEditStart}>✍️</button>
+          <h2>{family.name}</h2>
+          <button onClick={handleRemove}>ⓧ</button>
+        </>
+      )}
     </li>
   );
 }
