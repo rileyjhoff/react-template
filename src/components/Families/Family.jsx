7aefea71ee4fc9142af2e770d065747724ed391f
Riley Hoffman <rileyjhoff@gmail.com>
2022-08-02 15:28:27 -0700

    add ability to remove a family

--- src/components/Families/Family.jsx
+++ src/components/Families/Family.jsx
@@ -1,9 +1,15 @@
+import { useActions } from '../../state/hooks/families';
 import styles from './Family.css';
 
 export default function Family({ family }) {
+  const { remove } = useActions();
+  
+  const handleRemove = () => remove(family.id);
+
   return (
     <li className={styles.Family}>
       <h2>{family.name}</h2>
+      <button onClick={handleRemove}>ⓧ</button>
     </li>
   );
 }
